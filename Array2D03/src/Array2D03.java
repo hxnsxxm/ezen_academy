d178f8055b70799f9a3b2f666db598c7a4864ea8
EZEN <EZEN@DESKTOP-P5LT6UG>
2022-08-02 15:08:24 +0900

    j220801

--- Array2D03/src/Array2D03.java
+++ Array2D03/src/Array2D03.java
@@ -17,42 +17,59 @@
 
 public class Array2D03 {
 	public static void main(String[] args) {
+		// 학생들의 점수 저장, int형 2차원 배열 선언, 초기화
 		int[][] scores = {
-				{ 93, 93, 92},
-				{ 85, 85, 86},
-				{ 71, 71, 72},
-				{ 68, 68, 67},
-				{ 83, 83, 82},
+				{ 93, 93, 92, 0, 1},
+				{ 85, 85, 86, 0, 1},
+				{ 71, 71, 72, 0, 1},
+				{ 68, 68, 67, 0, 1},
+				{ 83, 83, 82, 0, 1},
 		};
-		/*
-		String[] subjects = {"국어", "영어", "수학"};
+		// 학생들의 평균을 저장, double형의 1차원 배열 선언
+		double[] avgs = new double[5];
 		
-		for (int i=0; i<scores.length; i++) {
-			int sum = 0;
-			double avg = 0.0;
-			System.out.println("***학생" + (i+1) + "의 점수***");
-			for (int j=0; j<scores[i].length; j++) {
-				System.out.print(subjects[j] + ": " +  scores[i][j] + "  ");
-				sum += scores[i][j];
+		// 학생들의 학점을 저장, char형 1차원 배열 선언
+		char[] grades = new char[5];
+		
+		// 학생들의 석차를 저장, int형의 1차원 배열 선언
+		// 석차는 1로 초기화하여 사용, 총점을 이용하여 석차 계산 ***************************
+
+		
+		// 총점 계산 및 저장, 평균 계산 및 저장, 학점 계산 및 저장 
+		for (int i=0; i<scores.length ;i++) {
+			for (int j=0; j<scores[i].length-2; j++) {
+				scores[i][3] += scores[i][j];    // 1. 각 학생의 총점
 			}
-			avg = (double)sum / scores[i].length;
-			System.out.println();
-			System.out.printf("총점: %d\n평균: %.2f\n\n", sum, avg);
+			avgs[i] = (double) scores[i][3] / 3; // 2. 각 학생의 평균
+			
+			switch((int)avgs[i]/10) {            // 3. 각 학생의 학점
+			case 10: case 9: grades[i] = 'A'; break;
+			case 8: grades[i] = 'B'; break;
+			case 7: grades[i] = 'C'; break;
+			case 6: grades[i] = 'D'; break;
+			default: grades[i] = 'F'; break;
+			}
 		}
-		*/
-		System.out.println("번호 | 국어 | 영어 | 수학 | 총점 |  평균     |");
-		System.out.println("--------------------------------");
+		
+		// 석차 계산 및 저장 
 		for (int i=0; i<scores.length; i++) {
-			int sum = 0;
-			double avg = 0.0;
+			for (int j=0; j<scores.length; j++) { // 4. 각 학생의 석차, i: 나, j: 상대방
+				if (scores[i][3] < scores[j][3]) {
+					++scores[i][4];
+				}
+			}
+		}
+
+		// 출력
+		System.out.println("번호 | 국어 | 영어 | 수학 | 총점 |  평균     |학점 |석차 |");
+		System.out.println("------------------------------------");
+		for (int i=0; i<scores.length; i++) {
 			System.out.print((i+1) + "번 ");
-			for (int j=0; j<scores[i].length; j++) {
+			for (int j=0; j<scores[i].length-2; j++) {
 				System.out.print("| " + scores[i][j] + " ");
-				sum += scores[i][j];
 			}
-			avg = (double) sum / scores[i].length;
-			System.out.printf("| %d | %.2f |\n", sum, avg);
-//			System.out.println("| " + sum + " | " + avg + " |");
+			System.out.printf("| %d | %.2f | %c | %d |\n", scores[i][3], avgs[i], grades[i], scores[i][4]);
 		}
+		
 	}
 }
